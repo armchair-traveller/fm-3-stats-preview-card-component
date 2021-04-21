586129597fcb6ef328b2604a66dd1ff4ba1b4a1e
armchair-traveller <sentient.sober.armchair@gmail.com>
2021-04-20 17:31:52 -0700

    add markup content

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,12 +3,21 @@
 </script>
 
 <svelte:head>
-  <!-- TODO: Title -->
+  <title>Frontend Mentor | Stats preview card component</title>
+
 </svelte:head>
 
 <main>
   <section>
     <h1>TODO: Add initial markup in src/routes/index.svelte</h1>
+  Get insights that help your business grow.
+
+  Discover the benefits of data analytics and make better decisions regarding revenue, customer 
+  experience, and overall efficiency.
+
+  10k+ companies
+  314 templates
+  12m+ queries
   </section>
 </main>
 
@@ -24,3 +33,5 @@
 
   /* TODO: Add your design breakpoints and additional details here, for reference while styling. */
 </style>
+
+
